69a0801884aaf9fdb14d02b6a09a80d4bbd0ce77
samuel8772 <kamauskk005@gmail.com>
2025-05-24 16:09:16 +0300

    inial commit

--- lib/person.py
+++ lib/person.py
@@ -17,3 +17,30 @@ APPROVED_JOBS = [
 
 class Person:
     pass
+
+    def __init__(self,job = "Admin",name = "guido"):
+        self.job = job
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self,value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self,value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs." )
+        
